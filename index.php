3c7c0fe49b838dc20767cfd398136078bd9d8ead
de034 <dejanpetrovic.vp@gmail.com>
2018-02-01 14:58:09 +0100

    ver 1
    ovo je pocetna verzija

---
+++ index.php
@@ -0,0 +1,226 @@
+					<header class="c-header-home -full" data-module="SliderHome" id="js-header">
+						<!-- slajdovi -->
+						<article class="c-header-home_section is-current js-slider-home-slide" data-slide="1">
+							<div class="c-header-home_background-parallax o-background js-parallax" data-speed="-1" data-position="top" data-target="#js-header">
+								<div class="c-header-home_background-load-wrap o-background">
+									<div class="c-header-home_background-load o-background">
+										<div class="c-header-home_background-wrap o-background">
+											<div class="c-header-home_background o-background">
+												<div class="c-header-home_image-wrap o-background">
+													<video title="1" id="bgvid" autoplay loop poster="assets/images/home_slider/s1.jpg" style="min-height: 100% !important;min-width: 100% !important;height: auto !important;width: auto !important;overflow: hidden;">
+
+														
+														<source src="mm.mp4" type="video/mp4">
+														
+														Your browser does not support the video tag.
+													
+													</video>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="c-header-home_text-load">
+								<div class="c-header-home_text">
+									<div class="c-header-home_container o-container js-parallax" data-speed="2" data-position="top" data-target="#js-header">
+										<h1 class="c-header-home_heading -full">SA NAMA DO ENERGIJE</h1>
+										<h3>GAT D.O.O. NOVI SAD</h3>
+										<!--<h3 class="c-header-home_heading2">Sa nama do energije</h3> -->
+										<a class="c-header-home_subheading -load o-hsub -link" href="istorija.php">
+											<span class="o-hsub_label">Detaljnije</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</article>
+						<article class="c-header-home_section is-next js-slider-home-slide" data-slide="2">
+							<div class="c-header-home_background-parallax o-background js-parallax" data-speed="-1" data-position="top" data-target="#js-header">
+								<div class="c-header-home_background-load-wrap o-background">
+									<div class="c-header-home_background-load o-background">
+										<div class="c-header-home_background-wrap o-background">
+											<div class="c-header-home_background o-background">
+												<div class="c-header-home_image-wrap o-background">
+													<div class="c-header-home_image o-background" style="background-image: url('assets/images/home_slider/s2.jpg');"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="c-header-home_text-load">
+								<div class="c-header-home_text">
+									<div class="c-header-home_container o-container js-parallax" data-speed="2" data-position="top" data-target="#js-header">
+										<h5 class="c-header-home_heading -full" style="font-size: 25pt">TRAFOSTANICE I DALEKOVODI</h5>
+										<h3>GAT D.O.O. NOVI SAD</h3>
+										<a class="c-header-home_subheading -load o-hsub -link" href="energetika2.php">
+											<span class="o-hsub_label">DETALJNIJE</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</article>
+						<article class="c-header-home_section is-next js-slider-home-slide" data-slide="3">
+							<div class="c-header-home_background-parallax o-background js-parallax" data-speed="-1" data-position="top" data-target="#js-header">
+								<div class="c-header-home_background-load-wrap o-background">
+									<div class="c-header-home_background-load o-background">
+										<div class="c-header-home_background-wrap o-background">
+											<div class="c-header-home_background o-background">
+												<div class="c-header-home_image-wrap o-background">
+													<div class="c-header-home_image o-background" style="background-image: url('assets/images/home_slider/s3.jpg');"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="c-header-home_text-load">
+								<div class="c-header-home_text">
+									<div class="c-header-home_container o-container js-parallax" data-speed="2" data-position="top" data-target="#js-header">
+									<!--	<h5 class="c-header-home_heading -full" style="font-size: 30pt">IZRADA I REKONSTRUKCIJA RUDARSKIH MAŠINA I SISTEMA, AUTOMATIZACIJA SISTEMA UPRAVLJANJA</h5> -->
+										
+										<h1 class="c-header-home_heading -full">RUDARSKE MAŠINE I SISTEMI</h1>
+										<h3>OPREMA I MAŠINE U RUDARSTVU</h3>
+										<a class="c-header-home_subheading -load o-hsub -link" href="rudarstvo.php">
+											<span class="o-hsub_label">DETALJNIJE</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</article>
+						<article class="c-header-home_section is-prev js-slider-home-slide" data-slide="4">
+							<div class="c-header-home_background-parallax o-background js-parallax" data-speed="-1" data-position="top" data-target="#js-header">
+								<div class="c-header-home_background-load-wrap o-background">
+									<div class="c-header-home_background-load o-background">
+										<div class="c-header-home_background-wrap o-background">
+											<div class="c-header-home_background o-background">
+												<div class="c-header-home_image-wrap o-background">
+													<div class="c-header-home_image o-background" style="background-image: url('assets/images/home_slider/s4.jpg');"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="c-header-home_text-load">
+								<div class="c-header-home_text">
+									<div class="c-header-home_container o-container js-parallax" data-speed="2" data-position="top" data-target="#js-header">
+									<!--	<h4 class="c-header-home_heading2" style="font-size: 23pt">Projektovanje i izrada nadzorno-upravljačkih ormana, MCC ormana, operaterskih pultova, razvoj softvera za PLC, HMI i SCADA sistem</h4> -->
+										<h1 class="c-header-home_heading -full">NADZORNO-UPRAVLJAČKI ORMANI</h1>
+										<h3>INDUSTRIJSKA AUTOMATIKA</h3>
+										<a class="c-header-home_subheading -load o-hsub -link" href="oupravljanja.php">
+											<span class="o-hsub_label">DETALJNIJE</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</article>
+						<article class="c-header-home_section is-prev js-slider-home-slide" data-slide="5">
+							<div class="c-header-home_background-parallax o-background js-parallax" data-speed="-1" data-position="top" data-target="#js-header">
+								<div class="c-header-home_background-load-wrap o-background">
+									<div class="c-header-home_background-load o-background">
+										<div class="c-header-home_background-wrap o-background">
+											<div class="c-header-home_background o-background">
+												<div class="c-header-home_image-wrap o-background">
+													<div class="c-header-home_image o-background" style="background-image: url('assets/images/home_slider/s5.jpg');"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="c-header-home_text-load">
+								<div class="c-header-home_text">
+									<div class="c-header-home_container o-container js-parallax" data-speed="2" data-position="top" data-target="#js-header">
+										<h1 class="c-header-home_heading -full">ELEKTRO-POSTROJENJA</h1>
+										<h3>ELEKTROMONTAŽNI I GRAĐEVINSKI RADOVI</h3>
+										<a class="c-header-home_subheading -load o-hsub -link" href="projektovanje.php">
+											<span class="o-hsub_label">DETALJNIJE</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</article>
+						<article class="c-header-home_section is-prev js-slider-home-slide" data-slide="6">
+							<div class="c-header-home_background-parallax o-background js-parallax" data-speed="-1" data-position="top" data-target="#js-header">
+								<div class="c-header-home_background-load-wrap o-background">
+									<div class="c-header-home_background-load o-background">
+										<div class="c-header-home_background-wrap o-background">
+											<div class="c-header-home_background o-background">
+												<div class="c-header-home_image-wrap o-background">
+													<div class="c-header-home_image o-background" style="background-image: url('assets/images/home_slider/s6.jpg');"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="c-header-home_text-load">
+								<div class="c-header-home_text">
+									<div class="c-header-home_container o-container js-parallax" data-speed="2" data-position="top" data-target="#js-header">
+										<h1 class="c-header-home_heading -full">OBJEKTI PO POTREBI KORISNIKA</h1>
+										<h3>VISOKOGRADNJA</h3>
+										<a class="c-header-home_subheading -load o-hsub -link" href="gradjevina.php">
+											<span class="o-hsub_label">DETALJNIJE</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</article>
+						<article class="c-header-home_section is-prev js-slider-home-slide" data-slide="7">
+							<div class="c-header-home_background-parallax o-background js-parallax" data-speed="-1" data-position="top" data-target="#js-header">
+								<div class="c-header-home_background-load-wrap o-background">
+									<div class="c-header-home_background-load o-background">
+										<div class="c-header-home_background-wrap o-background">
+											<div class="c-header-home_background o-background">
+												<div class="c-header-home_image-wrap o-background">
+													<div class="c-header-home_image o-background" style="background-image: url('assets/images/home_slider/s7.jpg');"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="c-header-home_text-load">
+								<div class="c-header-home_text">
+									<div class="c-header-home_container o-container js-parallax" data-speed="2" data-position="top" data-target="#js-header">
+										<h1 class="c-header-home_heading -full">REKONSTRUKCIJA GRAĐEVINSKIH OBJEKATA</h1>
+										<h3>VISOKOGRADNJA</h3>
+										<a class="c-header-home_subheading -load o-hsub -link" href="gradjevina.php">
+											<span class="o-hsub_label">DETALJNIJE</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</article>
+						<!-- strelice -->
+						<div class="c-header-home_footer">
+							<div class="o-container">
+								<div class="c-header-home_controls -nomobile o-button-group">
+									<div class="js-parallax" data-speed="1" data-position="top" data-target="#js-header">
+										<button class="o-button -white -square -left js-slider-home-button js-slider-home-prev" type="button">
+											<span class="o-button_label">
+												<svg class="o-button_icon" role="img"><use xlink:href="assets/images/slicice.svg#arrow-prev"></use></svg>
+											</span>
+										</button>
+										<button class="o-button -white -square js-slider-home-button js-slider-home-next" type="button">
+											<span class="o-button_label">
+												<svg class="o-button_icon" role="img"><use xlink:href="assets/images/slicice.svg#arrow-next"></use></svg>
+											</span>
+										</button>
+									</div>
+								</div>
+								<div class="c-header-home_buttons o-button-group">
+									<div class="js-parallax" data-speed="0.5" data-position="top" data-target="#js-header">
+										<a class="c-header-home_button o-button -white -left" data-route-option="keep-header" href="energetika.php">
+											<span class="o-button_label">Elektro sektor</span>
+										</a>
+										<a class="c-header-home_button o-button -white" data-route-option="keep-header" href="gradjevina.php">
+											<span class="o-button_label">Građevina</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</header>
+					
